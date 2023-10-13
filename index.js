1ecc94e2b4ed9308be51f43b6ea26ed85f6f6683
TBXark <tbxark@outlook.com>
2023-10-13 16:33:19 +0800

    perf: 调整代码结构

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-import { load } from 'cheerio'
+import { load } from "cheerio";
 
 /**
  * Converts a ReadableStream to an ArrayBuffer.
@@ -22,12 +22,11 @@ async function streamToArrayBuffer(stream, streamSize) {
     return result;
 }
 
-
 /**
  * Reads an email and extracts relevant information.
  *
  * @param {Uint8Array} raw - The raw email content.
- * @return {Object|null} - An object containing the extracted information: 
+ * @return {Object|null} - An object containing the extracted information:
  *   - from: The sender of the email.
  *   - to: The recipient of the email.
  *   - date: The date of the email.
@@ -38,16 +37,15 @@ async function readEmail(raw) {
     const PostalMime = require("postal-mime");
     const parser = new PostalMime.default();
     const parsedEmail = await parser.parse(raw);
-    const html = parsedEmail.html
-    const $ = load(html)
-    const table = $('table')
-    const from = table.find('tr').eq(0).find('td').eq(1).text()
-    const to = table.find('tr').eq(1).find('td').eq(1).text()
-    const date = table.find('tr').eq(2).find('td').eq(1).text()
-    const message = table.find('tr').eq(3).find('td').eq(1).text()
-    return { from, to, date, message }
+    const html = parsedEmail.html;
+    const $ = load(html);
+    const table = $("table");
+    const from = table.find("tr").eq(0).find("td").eq(1).text();
+    const to = table.find("tr").eq(1).find("td").eq(1).text();
+    const date = table.find("tr").eq(2).find("td").eq(1).text();
+    const message = table.find("tr").eq(3).find("td").eq(1).text();
+    return { from, to, date, message };
 }
-
 
 /**
  * Sends a message to a Telegram chat using the Telegram API.
@@ -58,36 +56,37 @@ async function readEmail(raw) {
  * @return {Promise<Object>} A promise that resolves to the response from the Telegram API.
  */
 async function sendMessageToTelegram(id, token, message) {
-    const req = await fetch(`https://api.telegram.org/bot${token}/sendMessage`, {
-        method: 'POST',
+    return await fetch(`https://api.telegram.org/bot${token}/sendMessage`, {
+        method: "POST",
         headers: {
-            'Content-Type': 'application/json'
+            "Content-Type": "application/json",
         },
         body: JSON.stringify({
             chat_id: id,
             text: message,
-        })
-    })
-    const res = await req.json()
-    return res
+        }),
+    });
 }
 
-
 async function fetchHandler(req, env, ctx) {
-    return new Response('It works!');
+    return new Response("It works!");
 }
 
 async function emailHandler(message, env, ctx) {
-    const { TELEGRAM_ID: id, TELEGRAM_TOKEN: token, EMAIL_WHITELIST: whitelist } = env
-    const whitelistArray = whitelist?.split(',') || []
+    const {
+        TELEGRAM_ID: id,
+        TELEGRAM_TOKEN: token,
+        EMAIL_WHITELIST: whitelist,
+    } = env;
+    const whitelistArray = whitelist?.split(",") || [];
     if (whitelistArray.length === 0) {
-        whitelistArray.push('voicemail@numberbarn.com')
+        whitelistArray.push("voicemail@numberbarn.com");
     }
     if (!whitelistArray.includes(message.from)) {
-        return
+        return;
     }
-    const raw = await streamToArrayBuffer(message.raw, message.rawSize)
-    const res = await readEmail(raw)
+    const raw = await streamToArrayBuffer(message.raw, message.rawSize);
+    const res = await readEmail(raw);
     const text = `
 From    : ${res.from}
 To      : ${res.to}
@@ -96,12 +95,11 @@ Date    : ${res.date}
 -----------
 
 ${res.message}
-`
-    await sendMessageToTelegram(id, token, text)
+`;
+    await sendMessageToTelegram(id, token, text);
 }
 
 export default {
     fetch: fetchHandler,
-    email: emailHandler
-}
-
+    email: emailHandler,
+};
